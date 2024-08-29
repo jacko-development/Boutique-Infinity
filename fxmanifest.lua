89d6749b5520bc66673f35aa75a81d0a59bf0f9e
atoshit <95099733+atoshit@users.noreply.github.com>
2024-08-30 01:23:33 +0200

    Add Main Folder and Files

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,3 +4,7 @@ games { 'gta5' }
 author 'Destructor & Atoshi'
 description 'Infinity Boutique for FiveM'
 version '1.0.0'
+
+shared_scripts { '@es_extended/imports.lua', 'Configs/*.lua'}
+server_scripts { '@oxmysql/lib/MySQL.lua', 'Server/*.lua' }
+client_scripts { 'Client/*.lua' }
