065aa3386b84d21776b747086107d76433e7e70f
atoshit <95099733+atoshit@users.noreply.github.com>
2024-08-30 17:19:33 +0200

    Command
    - Command givecoins add

--- Server/main.lua
+++ Server/main.lua
@@ -1,4 +1,4 @@
 print("[^5Infinity^7] [^2Boutique^7] Made with ^1❤^7  by ^3Atoshi^7 & ^3Destructor^7")
 
-local accountManager = PlayerAccountManager:new()
+accountManager = PlayerAccountManager:new()
 accountManager:onPlayerConnecting()
